8c674ecaf551baf0fdfb8134e8c113408988f52e
Juli Ramon <juli@nexumlabs.com>
2021-12-10 22:55:49 +0100

    add: picture banner citaprevia

--- components/CitaPrevia.js
+++ components/CitaPrevia.js
@@ -7,7 +7,7 @@ const CitaPrevia = () => {
   return (
     <Fade bottom cascade>
       <section className="relative py-12 md:py-20 bg-primary-300">
-        <div className="container mx-auto flex flex-wrap items-center justify-between">
+        <div className="container mx-auto flex flex-wrap items-center justify-center">
           <div className="w-full md:w-1/2">
             <span className="text-secondary-100 uppercase font-semibold">
               {GlobalTexts.brand}
@@ -25,17 +25,19 @@ const CitaPrevia = () => {
               </a>
             </div>
           </div>
-          <div className="w-full md:w-1/2">
-            <picture>
-              <source srcSet="../branding/clinica-dental-graell-cover-bg.jpg" />
-              <img
-                src="../branding/clinica-dental-graell-cover-bg.jpg"
-                datasrc="../branding/clinica-dental-graell-cover-bg.jpg"
-                alt={CommonTexts.citaprevia}
-                className="object-cover w-full h-full"
-                loading="lazy"
-              />
-            </picture>
+          <div className="w-full md:w-1/3 relative h-72 -mb-20">
+            <div className="absolute bottom-0 left-0 ">
+              <picture>
+                <source srcSet="../branding/dentist-dental-smile-girl-face-person-female-woman-transparent-png-163653.png" />
+                <img
+                  src="../branding/dentist-dental-smile-girl-face-person-female-woman-transparent-png-163653.png"
+                  datasrc="../branding/dentist-dental-smile-girl-face-person-female-woman-transparent-png-163653.png"
+                  alt={CommonTexts.citaprevia}
+                  className="object-cover w-full h-96"
+                  loading="lazy"
+                />
+              </picture>
+            </div>
           </div>
         </div>
       </section>
