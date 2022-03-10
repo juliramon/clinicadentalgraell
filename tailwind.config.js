5ca2f4c6e3675c6cab7e70fcece2b8d2e9f7f8ee
Juli Ramon <juli@nexumlabs.com>
2022-03-10 22:51:48 +0100

    fix: front --> twilwindcss not loading

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,36 @@
 module.exports = {
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     container: {
       padding: "4rem",
-    },
-    fontFamily: {
-      sans: ["Inter"],
-    },
-    colors: {
-      primary: {
-        100: "#ACD8E5",
-        200: "#4EAACE",
-        300: "#076899",
-        400: "#004f80",
-        500: "#082b3d",
-      },
-      secondary: {
-        100: "#ffffff",
-        200: "#f5f5f5",
-        300: "#ebebeb",
-        400: "#e1e1e1",
-        500: "#d7d7d7",
-        600: "#D1D0D1",
-        700: "#929295",
-      },
+      center: true,
     },
     extend: {
+      colors: {
+        primary: {
+          100: "#ACD8E5",
+          200: "#4EAACE",
+          300: "#076899",
+          400: "#004f80",
+          500: "#082b3d",
+        },
+        secondary: {
+          100: "#ffffff",
+          200: "#f5f5f5",
+          300: "#ebebeb",
+          400: "#e1e1e1",
+          500: "#d7d7d7",
+          600: "#D1D0D1",
+          700: "#929295",
+        },
+      },
+      fontFamily: {
+        sans: ["Inter"],
+      },
       borderRadius: {
         350: "350px",
       },
