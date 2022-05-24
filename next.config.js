66a6847b422568833485fe9dfb755b9cb0a4cc48
Juli Ramon <juli@nexumlabs.com>
2022-05-24 19:54:12 +0200

    add: front --> GA4 tracking

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
     SENDGRID_KEY: process.env.NEXT_PUBLIC_SENDGRID_KEY,
+    GOOGLE_ANALYTICS_TAG: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS,
   },
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import "../styles/globals.sass";
+import * as ga from "../lib/ga";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
 
